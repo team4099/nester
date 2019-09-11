b47923ee799eb7acdb8e084e890b13aab4b439f1
Jagan Prem <notjagan@gmail.com>
2019-09-10 20:33:08 -0400

    Add check for nested polygons

--- src/util.py
+++ src/util.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def line_intersection(p0, p1, q0, q1):
+def line_line_intersection(p0, p1, q0, q1):
     r = p1 - p0
     s = q1 - q0
     if np.isclose(np.cross(r, s), 0)[0]:
@@ -9,6 +9,16 @@ def line_intersection(p0, p1, q0, q1):
         t = (np.cross((q0 - p0), s) / np.cross(r, s))[0]
         u = (np.cross((q0 - p0), r) / np.cross(r, s))[0]
         return 0 <= t <= 1 and 0 <= u <= 1
+
+def ray_line_intersection_point(p0, r, q0, q1):
+    s = q1 - q0
+    if np.isclose(np.cross(r, s), 0)[0]:
+        return None
+    else:
+        t = (np.cross((q0 - p0), s) / np.cross(r, s))[0]
+        u = (np.cross((q0 - p0), r) / np.cross(r, s))[0]
+        if 0 <= t and 0 <= u <= 1:
+            return np.array([p0 + r * t, q0 + s * u])
 
 class Polygon:
     def __init__(self, vertices):
@@ -19,14 +29,30 @@ class Polygon:
         for i, (p0, p1) in enumerate(zip(self.vertices, self.vertices[1:] + \
             [self.vertices[0]])):
             for j, (q0, q1) in enumerate(zip(other.vertices,
-                other.vertices[1:] + [self.vertices[0]])):
-                if line_intersection(p0, p1, q0, q1):
+                other.vertices[1:] + [other.vertices[0]])):
+                if line_line_intersection(p0, p1, q0, q1):
                     intersections.append(i, j)
-        
+
         return intersections
 
     def is_nested(self, other):
-        pass
+        vertex = self.vertices[0]
+        total = 0
+        ray = np.array([1, 0])
+        for q0, q1 in zip(other.vertices, other.vertices[1:] + \
+            other.vertices[0]):
+            point = ray_line_intersection_point(vertex, ray, q0, q1)
+            if point is not None:
+                if np.isclose(point, q0).all():
+                    if q1[1] < point[1]:
+                        total += 1
+                elif np.isclose(point, q1).all():
+                    if q0[1] < point[1]:
+                        total += 1
+                else:
+                    total += 1
+
+        return total % 2 == 1
 
     def resolve_overlap(self, intersections):
         pass
